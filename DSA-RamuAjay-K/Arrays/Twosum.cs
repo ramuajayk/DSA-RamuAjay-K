81fa487c16868cffb6517f14ffe2320129be4c07
kramu <kramuajay@outlook.com>
2023-12-05 01:11:41 -0500

    Add project files.

---
+++ DSA-RamuAjay-K/Arrays/Twosum.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DSA_RamuAjay_K.Arrays
+{
+    internal class Twosum
+    {
+       
+        public int[] TwoNumberSumWithLookUp(int[] input, int target) 
+        {
+
+            HashSet<int> storeIteration = new HashSet<int>();
+            foreach (int i in input)
+            {
+                int sum = target - i;
+                if (storeIteration.Contains(sum))
+                {
+                   return new int[2]{sum, i};
+                }
+                else
+                {
+                    storeIteration.Add(i);  
+                }
+            }
+            return new int[2] { 0, 0 };
+        }
+
+        public int[] TwoNumberSumWithSlidingWindow(int[] input, int target)
+        {
+            Array.Sort(input);
+            
+        }
+
+
+    }
+}
---
+++ DSA-RamuAjay-K/Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using DSA_RamuAjay_K.Arrays;
+using System.Net.Http.Headers;
+
+Twosum prog = new Twosum();
+int[] input = new int[]{1, 34, 5};
+int target = 6;
+
+Console.WriteLine( prog.TwoNumberSum(input, target)[0]);
+Console.ReadLine();

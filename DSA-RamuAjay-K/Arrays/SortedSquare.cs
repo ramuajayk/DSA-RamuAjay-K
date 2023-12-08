46e67c4b7157c370174d875af33dcc82d0eb8839
kramu <kramuajay@outlook.com>
2023-12-08 12:48:31 -0500

    SortedSquare

---
+++ DSA-RamuAjay-K/Arrays/SortedSquare.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DSA_RamuAjay_K.Arrays
+{
+    internal class SortedSquare
+    {
+        public int[] SortedSquaredArray(int[] input)
+        {
+            int[] result = new int[input.Length];
+            for (int i = 0; i < input.Length; i++)            
+                result[i] = input[i] * input[i];           
+
+            Array.Sort(result);
+            return result;
+        }
+    }
+}

d81271e7ba98b4c70c83ed31ed02d8c84f7fd602
kramu <kramuajay@outlook.com>
2023-12-15 20:54:47 -0500

    NonConstructibleChange

---
+++ DSA-RamuAjay-K/Arrays/NonConstructibleChange.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DSA_RamuAjay_K.Arrays
+{
+    internal class NonConstructibleChange
+    {
+        public int ChangeCantCreate(int[] input)
+        {
+            int changeCantCreate = 0;
+            Array.Sort(input);
+            foreach (var coin in input) 
+            {
+                if (changeCantCreate + 1 < coin)
+                    return changeCantCreate + 1;
+
+                changeCantCreate += coin;
+            }
+            return changeCantCreate + 1;
+            
+             
+        }
+    }
+}

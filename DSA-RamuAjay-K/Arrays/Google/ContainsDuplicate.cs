18d9ab5d7498f46cbe8c8a9aaffeed8b49551979
kramu <kramuajay@outlook.com>
2024-01-04 20:45:07 -0500

    Contains Duplicate

---
+++ DSA-RamuAjay-K/Arrays/Google/ContainsDuplicate.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DSA_RamuAjay_K.Arrays.Google
+{
+    internal class ContainsDuplicate
+    {
+
+        public bool ContainsDuplicateValues(int[] array)
+        {
+
+            HashSet<int> result = new HashSet<int>();
+            foreach (int item in array)
+            {
+                if(!result.Add(item))
+                    return true;
+               
+            }
+            return false;
+        }
+    }
+}
